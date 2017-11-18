7fb972711c8c925931e9e76b7035aaadbf055927
Anithapraba Kathir <anithaprabakathir@gmail.com>
2017-11-18 19:11:58 +0530

    Update the responseHandler to use lambda expression

--- src/main/java/com/mycomp/eshop/clients/HttpClient.java
+++ src/main/java/com/mycomp/eshop/clients/HttpClient.java
@@ -2,24 +2,18 @@ package com.mycomp.eshop.clients;
 
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
+import org.eclipse.jetty.http.HttpStatus;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.URI;
 
 public class HttpClient {
-    private static int HTTP_SUCCESS = 200;
-    private static int HTTP_NOT_FOUND = 404;
-
     private CloseableHttpClient httpClient = null;
 
     public HttpClient() {
@@ -31,17 +25,13 @@ public class HttpClient {
         String responseBody = null;
 
         // Create a custom response handler
-        ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
-            @Override
-            public String handleResponse(
-                    final HttpResponse response) throws ClientProtocolException, IOException {
-                int status = response.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = response.getEntity();
-                    return entity != null ? EntityUtils.toString(entity) : null;
-                } else {
-                    throw new ClientProtocolException("Unexpected response status: " + status);
-                }
+        ResponseHandler<String> responseHandler = response -> {
+            int status = response.getStatusLine().getStatusCode();
+            if (status >= HttpStatus.OK_200 && status < HttpStatus.MULTIPLE_CHOICES_300) {
+                HttpEntity entity = response.getEntity();
+                return entity != null ? EntityUtils.toString(entity) : null;
+            } else {
+                throw new ClientProtocolException("Unexpected response status: " + status);
             }
         };
         try {
